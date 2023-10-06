6d98439bd914ee10c45113c5c851f1f58f4689bd
ElRapt <lucas.denis@etu.uca.fr>
2023-10-06 14:39:41 +0200

    Changed float and rename thisAmount

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class StatementPrinter {
 
   public String print(Invoice invoice, HashMap<String, Play> plays) {
-    int totalAmount = 0;
+    float totalAmount = 0;
     int volumeCredits = 0;
     StringBuffer result = new StringBuffer(String.format("Statement for %s\n", invoice.customer));
     // StringBuffer prevents creation of a new string object each concatenation
@@ -13,21 +13,21 @@ public class StatementPrinter {
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
+      float priceToPay = 0;
 
       switch (play.type) {
         case TRAGEDY:
-          thisAmount = 40000;
+          priceToPay = 400;
           if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            priceToPay += 10 * (perf.audience - 30);
           }
           break;
         case COMEDY:
-          thisAmount = 30000;
+          priceToPay = 300;
           if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            priceToPay += 100 + 5 * (perf.audience - 20);
           }
-          thisAmount += 300 * perf.audience;
+          priceToPay += 3 * perf.audience;
           break;
         default:
           throw new Error("unknown type: ${play.type}");
@@ -39,10 +39,10 @@ public class StatementPrinter {
       if (Play.PlayType.COMEDY.equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order
-      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
-      totalAmount += thisAmount;
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(priceToPay), perf.audience));
+      totalAmount += priceToPay;
     }
-    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
+    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount)));
     result.append( String.format("You earned %s credits\n", volumeCredits));
     return result.toString();
   }
