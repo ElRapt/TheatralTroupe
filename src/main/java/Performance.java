618b37b6c06fea8520ef2a089fe79921432a7e7f
Lucas Denis <lucas.denis@etu.uca.fr>
2023-10-25 14:29:05 +0200

    Added IllegalArgumentException in Performance

--- src/main/java/Performance.java
+++ src/main/java/Performance.java
@@ -4,6 +4,10 @@ public class Performance {
     public int audience;
 
     public Performance(Play play, int audience) {
+        if (audience < 0) {
+            throw new IllegalArgumentException("Audience cannot be negative");
+        }
+
         this.play = play;
         this.audience = audience;
     }
