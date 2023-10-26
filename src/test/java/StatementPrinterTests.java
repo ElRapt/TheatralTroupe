130150eebe8204b5e5df07b3fce467f1fd9143ea
Lucas Denis <lucas.denis@etu.uca.fr>
2023-10-26 10:52:48 +0200

    Added unit test for negative audience

--- src/test/java/StatementPrinterTests.java
+++ src/test/java/StatementPrinterTests.java
@@ -9,38 +9,45 @@ import java.util.List;
 public class StatementPrinterTests {
 
     Customer BigCo = new Customer("BigCo", UUID.randomUUID());
-  @Test
-  void testToTextInvoice(){
-      Play hamlet = new Tragedy("Hamlet");
-      Play asYouLikeIt = new Comedy("As You Like It");
-      Play othello = new Tragedy("Othello");
 
-      Invoice invoice = new Invoice(BigCo, List.of(
-              new Performance(hamlet, 55),
-              new Performance(asYouLikeIt, 35),
-              new Performance(othello, 40)
-      ));
+    @Test
+    void testToTextInvoice(){
+        Play hamlet = new Tragedy("Hamlet");
+        Play asYouLikeIt = new Comedy("As You Like It");
+        Play othello = new Tragedy("Othello");
 
-      String result = invoice.toText();
-      verify(result);
+        Invoice invoice = new Invoice(BigCo, List.of(
+                new Performance(hamlet, 55),
+                new Performance(asYouLikeIt, 35),
+                new Performance(othello, 40)
+        ));
+
+        String result = invoice.toText();
+        verify(result);
+    }
+
+    @Test
+    void testToHTMLInvoice(){
+        Play hamlet = new Tragedy("Hamlet");
+        Play asYouLikeIt = new Comedy("As You Like It");
+        Play othello = new Tragedy("Othello");
+
+        Invoice invoice = new Invoice(BigCo, List.of(
+                new Performance(hamlet, 55),
+                new Performance(asYouLikeIt, 35),
+                new Performance(othello, 40)
+        ));
+
+        String result = invoice.toHTML();
+        verify(result);
+    }
+
+    @Test
+    void testExceptionNegativeAudience(){
+        Play hamlet = new Tragedy("Hamlet");
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Performance(hamlet, -55);
+        });
+    }
   }
-
-  @Test
-  void testToHTMLInvoice(){
-      Play hamlet = new Tragedy("Hamlet");
-      Play asYouLikeIt = new Comedy("As You Like It");
-      Play othello = new Tragedy("Othello");
-
-      Invoice invoice = new Invoice(BigCo, List.of(
-              new Performance(hamlet, 55),
-              new Performance(asYouLikeIt, 35),
-              new Performance(othello, 40)
-      ));
-
-      String result = invoice.toHTML();
-      verify(result);
-  }
-
-  }
-
 
